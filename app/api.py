e8e02a6fe34c0f2f6594b8faedbe3db8535e937b
Adithya Menon R <adukottan@gmail.com>
2026-01-31 00:48:17 +0530

    fix: fixed auth routes

--- app/api.py
+++ app/api.py
@@ -2,5 +2,5 @@ from fastapi import APIRouter
 from app.routers import auth, system
 
 api_router = APIRouter()
-api_router.include_router(auth.router, tags=["Authentication"])
+api_router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 api_router.include_router(system.router, tags=["System"])
