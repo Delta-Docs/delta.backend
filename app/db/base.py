fc1051c91a423a06635883db0b7943d15f0d21dd
Adithya Menon R <adukottan@gmail.com>
2026-01-30 09:52:26 +0530

    feat: added initial schema in models

--- app/db/base.py
+++ app/db/base.py
@@ -1,1 +1,6 @@
 from app.db.base_class import Base
+from app.models.user import User
+from app.models.installation import Installation
+from app.models.repository import Repository, DocCoverageMap
+from app.models.drift import DriftEvent, DriftFinding, CodeChange
+
