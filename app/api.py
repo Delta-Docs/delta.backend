bc34d640e2ef89754c70b1064c7475a1e3c3c286
Adithya Menon R <adukottan@gmail.com>
2026-01-31 11:25:01 +0530

    feat: added webhook router

--- app/api.py
+++ app/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from app.routers import auth, system
+from app.routers import auth, system, webhooks
 
 api_router = APIRouter()
-api_router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 api_router.include_router(system.router, tags=["System"])
+api_router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+api_router.include_router(webhooks.router, prefix="/webhook", tags=["Webhooks"])
---
+++ app/routers/webhooks.py
@@ -0,0 +1,41 @@
+import hmac
+import hashlib
+from app.utils.config import settings
+from fastapi import APIRouter, Request, HTTPException, Depends
+from sqlalchemy.orm import Session
+
+from app.deps import get_db_connection
+from app.services.github_webhook_service import handle_github_event
+
+router = APIRouter()
+
+async def validate_github_signature(request: Request):
+    signature = request.headers.get("X-Hub-Signature-256")
+    if not signature:
+        raise HTTPException(status_code=403, detail="Missing signature")
+    
+    body = await request.body()
+
+    expected_signature = "sha256=" + hmac.new(
+        settings.GITHUB_WEBHOOK_SECRET.encode(), 
+        body, 
+        hashlib.sha256
+    ).hexdigest()
+    
+    if not hmac.compare_digest(signature, expected_signature):
+        raise HTTPException(status_code=403, detail="Invalid signature")
+    
+    return True
+
+@router.post("/github")
+async def github_webhook_handler(request: Request, db: Session = Depends(get_db_connection)):
+    await validate_github_signature(request)
+    payload = await request.json()
+    event_type = request.headers.get("X-GitHub-Event")
+    
+    try:
+        handle_github_event(db, event_type, payload)
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+        
+    return {"status": "Received and Processed Event"}
