25167572c678782064562d2da5bccdda92ca8edb
Adithya Menon R <adukottan@gmail.com>
2026-02-03 14:57:25 +0530

    feat: added repos endpoints

--- app/api.py
+++ app/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from app.routers import auth, system, webhooks
+from app.routers import auth, system, webhooks, repos
 
 api_router = APIRouter()
 api_router.include_router(system.router, tags=["System"])
 api_router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 api_router.include_router(webhooks.router, prefix="/webhook", tags=["Webhooks"])
+api_router.include_router(repos.router, prefix="/repos", tags=["Repositories"])
