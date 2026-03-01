6b52b03d336ae1e6a48aac9808db411ca8d52916
Adithya Menon R <adukottan@gmail.com>
2026-03-01 16:38:01 +0530

    feat: added endpoint to fetch, mark and delete notifications

--- app/api.py
+++ app/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.routers import auth, webhooks, repos, dashboard
+from app.routers import auth, webhooks, repos, dashboard, notifications
 
 # Main router
 api_router = APIRouter()
@@ -9,3 +9,4 @@ api_router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 api_router.include_router(webhooks.router, prefix="/webhook", tags=["Webhooks"])
 api_router.include_router(repos.router, prefix="/repos", tags=["Repositories"])
 api_router.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard"])
+api_router.include_router(notifications.router, prefix="/notifications", tags=["Notifications"])
